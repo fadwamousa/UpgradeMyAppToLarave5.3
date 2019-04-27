a416969b7e06393dc3827708cc82fbda881592c6
fadwa <fadwaana99@gmail.com>
2019-04-27 09:35:33 +0200

    factory all tables with relational

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -20,5 +20,63 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'role_id' => $faker->numberBetween(1,3)
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    static $password;
+    return [
+        'category_id' => $faker->numberBetween(1,10),
+        'photo_id'    => 1,
+        'title'       => $faker->sentence(7,11),
+        'body'        => $faker->paragraphs(rand(5,10),true),
+    ];
+});
+
+$factory->define(App\Role::class, function (Faker\Generator $faker) {
+    static $password;
+    return [
+        'name' => $faker->randomElement(['Admin','Moderator','author'])
+    ];
+});
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    static $password;
+    return [
+        'name' => $faker->randomElement(['PHP','CSS','LARAVEL','OOP'])
+    ];
+});
+
+
+$factory->define(App\Photo::class, function (Faker\Generator $faker) {
+    static $password;
+    return [
+        'file' => 'placeholder.jpg'
+    ];
+});
+
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+    static $password;
+    return [
+      'post_id'     => $faker->numberBetween(1,10),
+      'is_active'   =>  1,
+      'author'      => $faker->name,
+      'body'        => $faker->paragraphs(1,true),
+      'email'       => $faker->safeEmail,
+      'photo'       => 'placeholder.jpg'
+    ];
+});
+
+
+$factory->define(App\CommentReply::class, function (Faker\Generator $faker) {
+    static $password;
+    return [
+      'comment_id' => $faker->numberBetween(1,10),
+      'is_active'   =>  1,
+      'author'      => $faker->name,
+      'body'        => $faker->paragraphs(1,true),
+      'email'       => $faker->safeEmail,
+      'photo'       => 'placeholder.jpg'
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,16 +12,44 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-      factory(App\User::class,10)->create();
+      DB::statement('SET FOREIGN_KEY_CHECKS=0');
+      DB::table('users')->truncate();
+      DB::table('posts')->truncate();
+      DB::table('roles')->truncate();
+      DB::table('photos')->truncate();
+      DB::table('categories')->truncate();
+      DB::table('comments')->truncate();
+      DB::table('comment_replies')->truncate();
+
+      factory(App\User::class,10)->create()->each(function($user){
+
+        $user->posts()->save(factory(App\Post::class)->make());
+
+      });
+
+      factory(App\Role::class,3)->create();
+      factory(App\Category::class,4)->create();
+      factory(App\Photo::class,1)->create();
+      factory(App\Comment::class,15)->create()->each(function($comment){
+
+           $comment->replies()->save(factory(App\CommentReply::class)->make());
+
+      });
+
+      factory(App\CommentReply::class,20)->create();
+
+
 
     /*  factory(App\User::class,10)->create()->each(function($user){
 
         $user->posts()->save(factory(App\Post::class)->make());
 
-      });*/
+      });
+      //$this->call(UsersTableSeeder::class);
+      */
 
-        //$this->call(UsersTableSeeder::class);
-        //$this->call(CommentsTableSeeder::class);
+
+
 
     }
 }
